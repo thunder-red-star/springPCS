771f9c57d35f028e0053bb2a0bcd8de8c1207458
William <67127399+thunder-red-star@users.noreply.github.com>
2022-03-11 09:02:38 -0500

    rest of the catchup

--- 73_qsort/QuickSort.java
+++ 73_qsort/QuickSort.java
@@ -10,78 +10,84 @@
  *
  * 1. Summary of QuickSort algorithm:
  * QSort(arr):
- *
+ *    run partition on array, 
+	run partiion on each part of that partiitoned array
+		repeat until the low part is not less than the high
  * 2a. Worst pivot choice and associated run time:
- *
+ *     any that is element reliant Big O(n^2)
  * 2b. Best pivot choice and associated run time:
- *
+ *     L-R Big O(n^2)
  * 3. Approach to handling duplicate values in array:
- *
+ *    consistently view as smaller
  **/
 
 public class QuickSort
 {
-    //--------------v  HELPER METHODS  v--------------
-    //swap values at indices x, y in array o
-    public static void swap( int x, int y, int[] o )
-    {
-        int tmp = o[x];
-        o[x] = o[y];
-        o[y] = tmp;
+  //--------------v  HELPER METHODS  v--------------
+  //swap values at indices x, y in array o
+  public static void swap( int x, int y, int[] o )
+  {
+    int tmp = o[x];
+    o[x] = o[y];
+    o[y] = tmp;
+  }
+
+  //print input array
+  public static void printArr( int[] a )
+  {
+    for ( int o : a )
+      System.out.print( o + " " );
+    System.out.println();
+  }
+
+  //shuffle elements of input array
+  public static void shuffle( int[] d )
+  {
+    int tmp;
+    int swapPos;
+    for( int i = 0; i < d.length; i++ ) {
+      tmp = d[i];
+      swapPos = i + (int)( (d.length - i) * Math.random() );
+      swap( i, swapPos, d );
     }
+  }
 
-    //print input array
-    public static void printArr( int[] a )
-    {
-        for ( int o : a )
-            System.out.print( o + " " );
-        System.out.println();
+  //return int array of size s, with each element fr range [0,maxVal)
+  public static int[] buildArray( int s, int maxVal )
+  {
+    int[] retArr = new int[s];
+    for( int i = 0; i < retArr.length; i++ )
+      retArr[i] = (int)( maxVal * Math.random() );
+    return retArr;
+  }
+  //--------------^  HELPER METHODS  ^--------------
+
+
+
+
+  /**
+   * void qsort(int[])
+   * @param d -- array of ints to be sorted in place
+   */
+  public static void qsort( int[] d )
+  {
+    qsorthelper(0,d.length -1 ,d);
+  }
+
+  //you may need a helper method...
+  public static void qsorthelper (int low, int high, int[] arr) {
+    if (low < high) {
+      int pivpoint = Partition.partition(arr, low, high, low);
+      qsorthelper(low,pivpoint-1,arr);
+      qsorthelper(pivpoint+1,high,arr);
     }
-
-    //shuffle elements of input array
-    public static void shuffle( int[] d )
-    {
-        int tmp;
-        int swapPos;
-        for( int i = 0; i < d.length; i++ ) {
-            tmp = d[i];
-            swapPos = i + (int)( (d.length - i) * Math.random() );
-            swap( i, swapPos, d );
-        }
-    }
-
-    //return int array of size s, with each element fr range [0,maxVal)
-    public static int[] buildArray( int s, int maxVal )
-    {
-        int[] retArr = new int[s];
-        for( int i = 0; i < retArr.length; i++ )
-            retArr[i] = (int)( maxVal * Math.random() );
-        return retArr;
-    }
-    //--------------^  HELPER METHODS  ^--------------
+  }
 
 
 
-
-    /**
-     * void qsort(int[])
-     * @param d -- array of ints to be sorted in place
-     */
-    public static void qsort( int[] d )
-    {
-
-    }
-
-    //you may need a helper method...
-
-
-
-
-
-    //main method for testing
-    public static void main( String[] args )
-    {
-    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
+  //main method for testing
+  public static void main( String[] args )
+  {
 
     //get-it-up-and-running, static test case:
     int [] arr1 = {7,1,5,12,3};
@@ -107,10 +113,9 @@ public class QuickSort
     qsort( arrN );
     System.out.println("arrN after sort: " );
     printArr(arrN);
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
-    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
+
 
     //get-it-up-and-running, static test case w/ dupes:
     int [] arr2 = {7,1,5,12,3,7};
@@ -137,8 +142,9 @@ public class QuickSort
     qsort( arrMatey );
     System.out.println("arrMatey after sort: " );
     printArr(arrMatey);
+    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-    }//end main
+  }//end main
 
 }//end class QuickSort
