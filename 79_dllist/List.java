aee1685d64ba81c1d52130916933d5322e4119b5
William <67127399+thunder-red-star@users.noreply.github.com>
2022-03-17 23:01:10 -0400

    Create List.java

---
+++ 79_dllist/List.java
@@ -0,0 +1,9 @@
+public interface List
+{
+  public boolean add( String x );
+  public boolean add( int index, String newVal );
+  public String remove( int index );
+  public String get( int i );
+  public String set( int i, String x );
+  public int size();
+}
