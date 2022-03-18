dbb9c1dcf90c9bd34a664436f54669224731de13
thunder-red-star <wvongphanith30@stuy.edu>
2022-03-18 12:17:41 -0400

    qweqweqwe

---
+++ cobo/elevens/Activity2 Starter Code/DeckTester.java
@@ -0,0 +1,34 @@
+
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        Deck deck1 = new Deck({"Ukraine", "Belarus", "Russia"}, {"Tanks", "Battleships", "Aircraft"}, {5, 3, 100});
+        deck1.shuffle();
+        System.out.println(deck1);
+        System.out.println(deck1.deal());
+        System.out.println(deck1.deal());
+        System.out.println(deck1.deal());
+
+        Deck deck2 = new Deck({"America", "USA", "United States"}, {"Cheeseburgers", "Chicken Nuggets", "French Fries"}, {8, 2, 5});
+        deck2.shuffle();
+        System.out.println(deck2);
+        System.out.println(deck2.deal());
+        System.out.println(deck2.deal());
+        System.out.println(deck2.deal());
+
+        // [sic]
+        Deck deck3 = new Deck({"China", "Taiwan", "Japan"}, {"Buddhists", "Hinduists", "Juddists"}, {1, 2, 3});
+        deck3.shuffle();
+        System.out.println(deck3);
+        System.out.println(deck3.deal());
+        System.out.println(deck3.deal());
+        System.out.println(deck3.deal());
+    }
+}
