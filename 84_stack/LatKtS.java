6dd01c76ffcc653cc9876d4ef4867154e2fa099c
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-29 17:30:39 -0400

    new algorithm that works

--- 84_stack/LatKtS.java
+++ 84_stack/LatKtS.java
@@ -8,10 +8,9 @@
 
 import java.util.*;
 
-public class LatKtS
-{
+public class LatKtS {
 
-    public static boolean contains (String[] array, String value) {
+    public static boolean contains(String[] array, String value) {
         for (String character : array) {
             if (character.equals(value)) {
                 return true;
@@ -25,15 +24,14 @@ public class LatKtS
      * postcondition: returns reversed string s
      *                flip("desserts") -> "stressed"
      **/
-    public static String flip( String s )
-    {
+    public static String flip(String s) {
         String[] characters = s.split("");
         Latkes latke = new Latkes(1);
-        for (String character : characters ) {
+        for (String character : characters) {
             latke.push(character);
         }
         String reverse = "";
-        for (String character : characters ) {
+        for (String character : characters) {
             reverse += latke.pop();
         }
         return reverse;
@@ -46,51 +44,77 @@ public class LatKtS
      *                allMatched( "([)]" )        -> false
      *                allMatched( "" )            -> true
      **/
-    public static boolean allMatched( String s )
-    {
+    public static boolean allMatched(String s) {
         if (s.length() == 0) {
             return true;
         }
         // Assume no spaces
         if (s.length() % 2 != 0) {
             return false;
-        }
-        else {
+        } else {
             String[] openers = {"{", "(", "["};
             String[] closers = {"}", ")", "]"};
-            // Latkes is a stack
+
             Latkes latke = new Latkes(1);
-            for (String character : s.split("")) {
-                if (contains(openers, character)) {
-                    latke.push(character);
+
+            int parenthesisCount = 0;
+            int bracketCount = 0;
+            int braceCount = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                String character = s.substring(i, i + 1);
+                // Openers
+                if (character.equals("{")) {
+                    latke.push("{");
+                    braceCount++;
+                } else if (character.equals("[")) {
+                    latke.push("[");
+                    bracketCount++;
+                } else if (character.equals("(")) {
+                    latke.push("(");
+                    parenthesisCount++;
+                    // Closers
+                } else if (character.equals("}")) {
+                    if (latke.pop().equals("{")) {
+                        braceCount--;
+                    }
+                } else if (character.equals("]")) {
+                    if (latke.pop().equals("[")) {
+                        bracketCount--;
+                    }
+                } else if (character.equals(")")) {
+                    if (latke.pop().equals("(")) {
+                        parenthesisCount--;
+                    }
+                } else {
+                    System.out.println("Passing character " + character + "as it is not a parenthesis");
                 }
-                else if (contains(closers, character)) {
-                    if (latke.isEmpty()) {
-                        return false;
-                    }
-                    else if (latke.peek().equals(character)) {
-                        latke.pop();
-                    }
-                    else {
-                        return false;
-                    }
+
+                // Escape early if there are more closer than openers
+                if (braceCount < 0 || parenthesisCount < 0 || bracketCount < 0) {
+                    return false;
                 }
             }
-            return latke.isEmpty();
+
+            // All matched?
+            if (braceCount == 0 && parenthesisCount == 0 && bracketCount == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
 
     //main method to test
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         System.out.println(flip("stressed"));
-        System.out.println(allMatched( "({}[()])" )); //true
-        System.out.println(allMatched( "([)]" ) ); //false
-        System.out.println(allMatched( "(){([])}" ) ); //true
-        System.out.println(allMatched( "](){([])}" ) ); //false
-        System.out.println(allMatched( "(){([])}(" ) ); //false
-        System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
+        System.out.println(allMatched("({}[()])")); //true
+        System.out.println(allMatched("([)]")); //false
+        System.out.println(allMatched("(){([])}")); //true
+        System.out.println(allMatched("](){([])}")); //false
+        System.out.println(allMatched("(){([])}(")); //false
+        System.out.println(allMatched("()[[]]{{{{((([])))}}}}")); //true
     }
 
 }//end class
