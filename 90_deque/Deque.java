4309e56e0782e01130e901043f9834de27c70047
ThunderRedStar <wvongphanith30@stuy.edu>
2022-04-12 16:33:34 +0000

    achoo

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -1,37 +1,9 @@
-import java.util.LinkedList;
-
-public class Deque<Item> {
-    private LinkedList<Item> _data;
-
-    public Deque() {
-	_data = new LinkedList<Item>();
-    }
-
-    public boolean isEmpty() {
-	return _data.isEmpty();
-    }
-
-    public int size() {
-	return _data.size();
-    }
-
-    public void addFirst(Item item) {
-	_data.addFirst(item);
-    }
-
-    public void addLast(Item item) {
-	_data.addLast(item);
-    }
-
-    public Item removeFirst() {
-	return _data.removeFirst();
-    }
-
-    public Item removeLast() {
-	return _data.removeLast();
-    }
-
-    public Item getFirst () {
-	return _data.getFirst();
-    }
+public interface Deque<Item> {
+    void addFirst(Item item);
+    void addLast(Item item);
+    Item removeFirst();
+    Item removeLast();
+    Item getFirst();
+    int size();
+    boolean isEmpty();
 }
